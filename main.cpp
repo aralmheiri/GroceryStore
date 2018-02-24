8df4e08e2e8e7cc0008148d48de507f62d47078e
aralmheiri <aralmheiri@mail.csuchico.edu>
2018-02-23 17:01:07 -0800

    arrays

--- main.cpp
+++ main.cpp
@@ -1,19 +1,40 @@
+//Author: Abdulla Almheiri
 #include<iostream>
 #include<string>
 using namespace std;
-
 int main()
 {
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
-
-  cout<<"\n==GROCERY LIST MANAGER==";
+  string input2;
+  do
+{
+  cout<<"\n==GROCERY LIST MANAGER=="; 
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
+  if(input =='A' || input == 'a')
+  {
+     cout<<"what is the item?\n";
+     cin>>input2;
+  }
+  if (numItems < 5)
+  {
+     list[numItems]=input2;
+     numItems++;
+  }
+  else 
+  {
+     cout<<"You'll need a bigger list!\n";
+  }
 
-  return 0;
 }
+
+ while(input!='q' && input!='Q');
+
+return 0;
+}
+
